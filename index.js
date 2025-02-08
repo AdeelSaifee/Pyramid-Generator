a1cf952083f622cdcfab5e80ca3eb6e04ba76f91
adeel <adeel.saifee822@gmail.com>
2025-02-08 20:59:00 +0500

    Updated

--- index.js
+++ index.js
@@ -1,27 +1,41 @@
-const character = "#";
+const character = '#';
 const count = 8;
+const rows = new Array(count);
 let inverted = false;
-const rows = [];
+
+
+function manualRepeat(char, times) {
+    let result = '';
+    for (let i = 0; i < times; i++) {
+        result = result + char;
+    }
+    return result;
+}
+
 
 function padRow(rowNumber, rowCount) {
-    return " ".repeat(rowCount - rowNumber) + character.repeat(2*rowNumber-1) + " ".repeat(rowCount - rowNumber);
+    const spaces = manualRepeat(' ', rowCount - rowNumber);
+    const hashes = manualRepeat(character, 2 * rowNumber - 1);
+    return spaces + hashes + spaces;
 }
 
-for(let i=1;i<=count;i++) {
-    if(inverted){
-        rows.unshift(padRow(i,count));
+
+for (let i = 0; i < count; i++) {
+    if (inverted) {
+        for (let j = count - 1; j > 0; j--) {
+            rows[j] = rows[j - 1];
+        }
+        rows[0] = padRow(i+1,count);
+    } else {
+        rows[i] = padRow(i+1,count);
     }
-    else{
-        rows.push(padRow(i,count));
-    }
 }
 
-/*while(rows.length<count){
-    rows.push(padRow(rows.length+1, count));
-}*/
-let result = "";
 
-for(const row of rows) {
-    result += row + "\n";
+let result = '';
+for (let k = 0; k < count; k++) {
+    result = result + rows[k] + '\n';
 }
+
 console.log(result);
+
